b30f0f747e49a349fe1b40ab5b50df05efbc4087
Hawaiii <yihua25@gmail.com>
2016-05-15 15:22:48 -0700

    class 1 from stanford cs193 iOS 9 app development

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInMiddleOfTyping = false
+    
+    @IBAction func touchDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInMiddleOfTyping{
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInMiddleOfTyping = true
     }
+ 
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func performOperation(sender: UIButton) {
+        userIsInMiddleOfTyping = false
+        if let mathSymbol = sender.currentTitle {
+            if mathSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
     }
-
-
 }
 
